28319eb9e5d9febb658540c22b967390ec7f8d3f
AlexLTK1 <96800981+AlexLTK1@users.noreply.github.com>
2022-06-14 06:43:23 -0700

    june 14

--- Javascript solutions/Arrays/448. Find All Numbers Disappeared in an Array.js
+++ Javascript solutions/Arrays/448. Find All Numbers Disappeared in an Array.js
@@ -3,13 +3,13 @@
  * @return {number[]}
  */
  var findDisappearedNumbers = function(nums) {
-    let unseen = [];
-    for (let i = 1; i < nums.length + 1; i++) {
-        if (!nums.includes(i)) {
-            unseen.push(i);
-        }
-    } return unseen;
+    let set = new Set(Array.from({length: nums.length}, (v,i) => i+1));
+    for (let i = 0; i < nums.length; i++) {
+        set.delete(nums[i]);
+    } return Array.from(set.values());
     
 };
 
 findDisappearedNumbers([1,2,2,3]);
+let nums = [1,2,2,3];
+console.log(Array.from({length: nums.length}, k => k+2));
