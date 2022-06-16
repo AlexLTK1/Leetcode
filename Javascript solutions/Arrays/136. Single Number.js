74f9cc6076f4a81fc9cdf779d9a24d1b88c232be
AlexLTK1 <96800981+AlexLTK1@users.noreply.github.com>
2022-06-15 23:47:45 -0700

    june 15

---
+++ Javascript solutions/Arrays/136. Single Number.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var singleNumber = function(nums) {
+    // create a hashmap, then store key value as how many times we've seen each nums
+    // return the single one.
+};
